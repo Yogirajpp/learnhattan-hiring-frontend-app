25e79990e38ec00042538f02308e36a37eeda8ee
Shreed95 <116705041+Shreed95@users.noreply.github.com>
2025-02-08 03:38:35 +0530

    Dashboard Completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,12 +21,7 @@ const data = {
     { title: "Projects", url: "/projects", icon: Bot },
     { title: "Leaderboard", url: "/leaderboard", icon: BookOpen },
     { title: "Jobs", url: "/jobs", icon: Settings2 },
-  ],
-  projects: [
-    { name: "Design Engineering", url: "#", icon: Frame },
-    { name: "Sales & Marketing", url: "#", icon: PieChart },
-    { name: "Travel", url: "#", icon: Map },
-  ],
+  ]
 };
 
 const SidebarComponent = () => {
@@ -73,23 +68,6 @@ const SidebarComponent = () => {
                       <item.icon />
                     </div>}
                     <span>{item.title}</span>
-                  </SidebarMenuButton>
-                </Link>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-
-          {/* Projects Section */}
-          <h2 className={`px-2 py-2 text-2md ${isCollapsed ? 'hidden' : 'block'}`}>Projects</h2>
-          <SidebarMenu>
-            {data.projects.map((project) => (
-              <SidebarMenuItem key={project.name}>
-                <Link to={project.url}>
-                  <SidebarMenuButton className="flex items-center text-md h-8"> {/* Increased text size */}
-                    {project.icon && <div className="justify-center w-5 h-5 mr-3"> {/* Increased icon size */}
-                      <project.icon />
-                    </div>}
-                    <span>{project.name}</span>
                   </SidebarMenuButton>
                 </Link>
               </SidebarMenuItem>
